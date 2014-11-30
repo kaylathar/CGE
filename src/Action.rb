a85913faba41ef5a0904b4171ae3903af8d046df
Kayla McArthur <kayla@kayla.is>
2014-11-30 01:12:26 -0800

    Add initial action support

--- src/Action.rb
+++ src/Action.rb
@@ -2,5 +2,8 @@ require_relative 'Configurable'
 class Action
   extend Configurable
 
-
+  def activate(options)
+    process_options(options)
+    yield invoke 
+  end
 end
---
+++ src/actions/EmailAction.rb
@@ -0,0 +1,25 @@
+require_relative "../Action"
+
+class EmailAction < Action
+  has_option :to, String
+  has_option :from, String
+  has_option :subject, String
+  has_option :body, String
+
+  def invoke
+    message = format_email(@to.value,@from.value,@subject.value,@body.value)
+    # Send using Net::SMTP or sendmail or whatever, for now log
+    puts message
+    true
+  end
+
+  def format_email(to,from,subject,body)
+    messages = <<TEXT
+From: #{from}
+To: #{to}
+Subject: #{subject}
+
+#{body}
+TEXT
+  end
+end

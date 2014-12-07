07c4476d96b07daa7c48feb108b87d9b1c2e19e0
Kayla McArthur <kayla@kayla.is>
2014-12-07 11:48:55 -0800

    Fix rubocop violations

--- src/configurable.rb
+++ src/configurable.rb
@@ -11,22 +11,21 @@
 # each option, and a options class method that returns the options
 # this class supports in the form of a hash
 module Configurable
-
   def process_options(options)
-    options.each do |key,value|
+    options.each do |key, value|
       key = key.to_s
-      raise OptionException, "Invalid option #{key}" unless self.class.options[key]
-      opt = self.send("#{key}")
-      opt.value = value;
-      raise OptionException, "Invalid option value for option #{key}" unless opt.is_valid?
+      fail OptionException, "Invalid option #{key}" unless self.class.options[key]
+      opt = send("#{key}")
+      opt.value = value
+      fail OptionException, "Invalid option value for option #{key}" unless opt.is_valid?
     end
     validate_required_options
   end
 
   def validate_required_options
-    self.class.send("required_options").each do |name|
-            opt = send("#{name}")
-            raise OptionException, "Required option #{name} missing or invalid" unless opt.is_valid?
+    self.class.send('required_options').each do |name|
+      opt = send("#{name}")
+      fail OptionException, "Required option #{name} missing or invalid" unless opt.is_valid?
     end
   end
 
@@ -39,77 +38,73 @@ module Configurable
 
   module ClassMethods
     def setup_options
-      begin
-        class_variable_get("@@options")
-        class_variable_get("@@required_options")
-      rescue
-        class_variable_set("@@options",Hash.new)
-        class_variable_set("@@required_options",Array.new)
-      end
+      class_variable_get('@@options')
+      class_variable_get('@@required_options')
+    rescue
+      class_variable_set('@@options', {})
+      class_variable_set('@@required_options', [])
     end
 
     def setup_option(name, type, required, verifier)
       define_method("#{name}") do
-        instance_variable_set("@"+name, Option.new(name, type, verifier)) unless instance_variable_get("@"+name)
-        instance_variable_get("@"+name)
+        instance_variable_set('@' + name, Option.new(name, type, verifier)) unless instance_variable_get('@' + name)
+        instance_variable_get('@' + name)
       end
 
       if required == :required
-        class_variable_get("@@required_options") << name
+        class_variable_get('@@required_options') << name
       end
 
-      class_variable_get("@@options")[name] = type
+      class_variable_get('@@options')[name] = type
     end
 
     def setup_outputs
-      begin
-        class_variable_get("@@outputs")
-      rescue
-        class_variable_set("@@outputs",Hash.new)
-      end 
+      class_variable_get('@@outputs')
+    rescue
+      class_variable_set('@@outputs', {})
     end
 
     def setup_output(name, type)
       define_method("#{name}") do
         instance_variable_get("@#{name}")
       end
-      class_variable_get("@@outputs")[name]=type
+      class_variable_get('@@outputs')[name] = type
     end
 
     def attr_option(name, type, required = :optional, &verifier)
       name = name.to_s
-      setup_options    
-      setup_option(name,type,required,verifier) 
+      setup_options
+      setup_option(name, type, required, verifier)
     end
 
     def attr_output(name, type)
       name = name.to_s
       setup_outputs
-      setup_output(name,type)
+      setup_output(name, type)
     end
 
     # Returns required set of options
     # @return [Array<Option>] Required options for this class
     def required_options
-      class_variable_get("@@required_options")
+      class_variable_get('@@required_options')
     end
 
     # Returns valid oset of options
     # @return [Hash<String,Class>] Available set of options, with expected
     # class for each.
     def options
-      class_variable_get("@@options")
+      class_variable_get('@@options')
     end
 
     # Returns set of outputs that are set
     # @return [Hash<String,Class>]] Outputs that are set on trigger, with
     # types of each as values
     def outputs
-      class_variable_get("@@outputs")
+      class_variable_get('@@outputs')
     end
 
     protected :attr_option, :attr_output
-    #private :setup_options, :setup_option, :setup_output
+    private :setup_options, :setup_option, :setup_output, :setup_outputs
   end
 end
 
@@ -119,23 +114,23 @@ end
 # intentional, as there may be cases where you can set an invalid
 # option value
 class Option
-  attr_reader :name,:type
+  attr_reader :name, :type
   attr_accessor :value
 
-  def initialize(name,type,verifier = nil,&block_verifier)
-    @type = type;
-    @name = name;
+  def initialize(name, type, verifier = nil, &block_verifier)
+    @type = type
+    @name = name
     @verifier = if verifier
-      verifier
-    elsif block_verifier
-      block_verifier
-    else
-      true
+                  verifier
+                elsif block_verifier
+                  block_verifier
+                else
+                  true
     end
   end
 
   def is_valid?
-    @value != nil && @value.is_a?(@type) && (@verifier == true || @verifier.call(@value))
+    !@value.nil? && @value.is_a?(@type) && (@verifier == true || @verifier.call(@value))
   end
 end
 

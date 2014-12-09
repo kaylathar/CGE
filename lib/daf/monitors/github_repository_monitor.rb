233b970e55b59ff7c8d4e44993b429acc4fa655c
Kayla McArthur <kayla@kayla.is>
2014-12-08 17:57:28 -0800

    Add github monitor

---
+++ lib/daf/monitors/github_repository_monitor.rb
@@ -0,0 +1,44 @@
+require 'daf/monitor'
+require 'net/http'
+require 'json'
+
+module DAF
+  # Monitors a github repository for updates at periodic intervals
+  class GithubRepositoryMonitor < Monitor
+    attr_option :owner, String, :required
+    attr_option :repo, String, :required
+    attr_option :branch, String, :required
+    attr_option :frequency, Integer, :required
+    attr_output :sha, String
+
+    def block_until_triggered
+      head_sha = github_head_sha(@owner, @repo, @branch)
+      loop do
+        sleep @frequency
+        new_sha = github_head_sha(@owner, @repo, @branch)
+        next unless new_sha != head_sha
+        @sha = new_sha
+      end
+    end
+
+    def github_head_sha(owner, repo, branch)
+      uri = refs_uri(owner, repo, branch)
+      begin
+        response_body = Net::HTTP.get_response(URI.parse(uri)).body
+        parse_commit_sha(response_body)
+      rescue
+        # Replace with actual logger in future...
+        STDERR.puts 'HTTP connection failed'
+      end
+    end
+
+    def parse_commit_sha(body)
+      response = JSON.parse(body)
+      response['object']['sha']
+    end
+
+    def refs_uri(owner, repo, branch)
+      "https://api.github.com/repos/#{owner}/#{repo}/git/refs/heads/#{branch}"
+    end
+  end
+end

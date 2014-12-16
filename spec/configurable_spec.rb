e7111f476a8308e5e94853bfebcc1f21d5770a82
Kayla McArthur <kayla@kayla.is>
2014-12-16 12:17:33 -0800

    Update spec tests

--- spec/configurable_spec.rb
+++ spec/configurable_spec.rb
@@ -19,6 +19,44 @@ describe DAF::Configurable do
   end
 
   it 'has required option' do
+    expect{MockClass.required_options}.to_not raise_error
     expect(MockClass.required_options).not_to be_empty
+    expect(MockClass.required_options.length).to eq(1)
+  end
+
+  it 'has options' do
+    expect{MockClass.options}.to_not raise_error
+    expect(MockClass.options).not_to be_empty
+    expect(MockClass.options.length).to eq(2)
+  end
+
+  it 'has outputs' do
+    expect{MockClass.outputs}.to_not raise_error
+    expect(MockClass.outputs).not_to be_empty
+    expect(MockClass.outputs.length).to eq(2)
+  end
+
+  it 'has readable outputs' do
+    expect{@under_test.test2_out}.to_not raise_error
+    expect{@under_test.test_out}.to_not raise_error
+  end
+
+  it 'exposes required type for inputs' do
+    expect(@under_test.test.type).to eq(String)
+    expect(@under_test.test2.type).to eq(Integer)
+  end
+
+  it 'has writable inputs' do
+    @under_test.test.value = 'Test'
+    @under_test.test2.value = 40
+    expect(@under_test.test.value).to eq('Test')
+    expect(@under_test.test2.value).to eq(40)
+  end
+
+  it 'validates inputs' do
+    @under_test.test.value = 40
+    @under_test.test2.value = "bad value"
+    expect(@under_test.test.valid?).to eq(false)
+    expect(@under_test.test.valid?).to eq(false)
   end
 end

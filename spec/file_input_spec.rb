aca4679476d8ea8c62c3c8f7669c4cc8561a868b
Kayla McArthur <kayla@kayla.is>
2025-06-21 08:33:09 +0100

    Add file input node
    Creates a new file input node that can read a local file into the command graph's output context for other nodes to use.

---
+++ spec/file_input_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+require 'tempfile'
+
+describe DAF::FileInput do
+  let(:file_input) { DAF::FileInput.new }
+  let(:temp_file) { Tempfile.new('test_file') }
+  let(:test_content) { 'Hello, World!' }
+  let(:options) { { 'file_path' => temp_file.path } }
+
+  before do
+    temp_file.write(test_content)
+    temp_file.close
+  end
+
+  after do
+    temp_file.unlink
+  end
+
+  it 'should read file content correctly' do
+    file_input.process(options)
+    expect(file_input.content).to eq(test_content)
+  end
+
+  it 'should raise error when file_path is not provided' do
+    expect { file_input.process({}) }
+      .to raise_error(DAF::OptionError)
+  end
+
+  it 'should raise error when file_path is empty' do
+    expect { file_input.process({ 'file_path' => '' }) }
+      .to raise_error(DAF::OptionError)
+  end
+
+  it 'should raise error when file does not exist' do
+    expect { file_input.process({ 'file_path' => '/nonexistent/file.txt' }) }
+      .to raise_error(DAF::OptionError)
+  end
+
+  it 'should raise error when path is a directory' do
+    Dir.mktmpdir do |dir|
+      expect { file_input.process({ 'file_path' => dir }) }
+      .to raise_error(DAF::OptionError)
+    end
+  end
+
+  it 'should raise error when file is not readable' do
+    File.chmod(0o000, temp_file.path)
+    expect { file_input.process(options) }
+      .to raise_error(DAF::OptionError)
+  ensure
+    File.chmod(0o644, temp_file.path)
+  end
+
+  it 'should handle large files' do
+    large_content = 'A' * 10000
+    large_file = Tempfile.new('large_test_file')
+    large_file.write(large_content)
+    large_file.close
+
+    file_input.process({ 'file_path' => large_file.path })
+    expect(file_input.content).to eq(large_content)
+
+    large_file.unlink
+  end
+
+  it 'should handle binary files' do
+    binary_content = "\x00\x01\x02\xFF"
+    binary_file = Tempfile.new('binary_test_file', binmode: true)
+    binary_file.write(binary_content)
+    binary_file.close
+
+    file_input.process({ 'file_path' => binary_file.path })
+    expect(file_input.content).to eq(binary_content)
+
+    binary_file.unlink
+  end
+
+  it 'should handle empty files' do
+    empty_file = Tempfile.new('empty_test_file')
+    empty_file.close
+
+    file_input.process({ 'file_path' => empty_file.path })
+    expect(file_input.content).to eq('')
+
+    empty_file.unlink
+  end
+end
